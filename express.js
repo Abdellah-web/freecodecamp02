a4cd7d7c9371a633c62707c50722073486e1c8f0
Abdellah-web <54070353+Abdellah-web@users.noreply.github.com>
2019-08-16 19:40:52 +0200

    Create express.js

---
+++ express.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const app = express();
+const body-parser = require('body-parser');
+const api = require(./request.js);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+app.enable('trust proxy');
+
+var port = process.env.PORT || 8080;
+
+api(app);
+
+app.listen(port, function(){
+    console.log('listening on port ' + port);
+    
+})
